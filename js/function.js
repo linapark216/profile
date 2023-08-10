43209c5570827ab209477d9f3a8dcbf8ecf45df4
lina <orangepop9005@gmail.com>
2023-08-10 15:44:14 +0900

    intro 인사말 작성

--- js/function.js
+++ js/function.js
@@ -1,5 +1,26 @@
+//loading
 $(function () {
 	const $loading = $('.loading');
-	$loading.children('p').fadeOut(1000);
-	$loading.delay(500).fadeOut(1000);
+	$loading.children('p').fadeOut(2000);
+	$loading.delay(1000).fadeOut(2000);
+});
+
+//
+$(function () {
+	const $home = $('#home');
+
+	/*
+        브라우저 화면의 크기
+        
+        1) 스크롤바와 툴바를 포함하지 않은 브라우저 화면의 크기
+            window.innerWidth
+            window.innerHeight
+        2) 스크롤바와 툴바를 포함한 브라우저 화면의 크기
+            window.outerWidth
+            window.outerHeight
+    */
+
+	$(window).on('load resize', function () {
+		$home.height(window.innerHeight);
+	});
 });
